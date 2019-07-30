670a230b288621b2270832a5af67f2635f405868
Priscila Martinez <priscila.mtz.fraire@gmail.com>
2019-07-30 09:59:05 -0500

    Header

--- Organizing-JS-Functionality/js/header.js
+++ Organizing-JS-Functionality/js/header.js
@@ -1,1 +1,18 @@
-// put event handlers for header links here
+$(document).ready(function() {
+    const $controls = $('[rel="js-controls"]');
+    const $modal = $('#modal');
+
+    $controls.on('click', 'a', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        e.stopImmediatePropagation();
+
+        const $target = $(e.target);
+        const url = $target.attr('href');
+
+        $.ajax(url, { dataType: 'text' })
+        .then(function(contents) {
+            $modal.html(contents).show();
+        });
+    });
+});
