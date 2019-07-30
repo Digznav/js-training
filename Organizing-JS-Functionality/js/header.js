c25d63914dc5f170af56a84c962a271d84d90c43
ZoraidaVallejo <zoraida.vallejo@justia.com>
2019-07-30 09:56:37 -0500

    Task 1

--- Organizing-JS-Functionality/js/header.js
+++ Organizing-JS-Functionality/js/header.js
@@ -1,1 +1,21 @@
 // put event handlers for header links here
+$(document).ready(function(){
+
+  var $modal = $("[rel='js-modal']");
+
+  //$("[rel*='js-register'], [rel*='js-login']")
+  $("[rel='js-header']").on("click", "[rel*='js-']", function(evt){
+    evt.preventDefault();
+    evt.stopPropagation();
+    evt.stopImmediatePropagation();
+
+    var url = $(evt.target).attr("href");
+
+    $.ajax(url, {dataType: "text"})
+    .then(function(contents){
+      $modal.html(contents).show();
+    });
+
+  })
+
+});
