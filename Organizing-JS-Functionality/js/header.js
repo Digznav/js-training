04f677464fcb034b950ee5fc4db0d18959348d8a
Diego Miguel <digznav@gmail.com>
2019-07-30 10:11:53 -0500

    Task 1

--- Organizing-JS-Functionality/js/header.js
+++ Organizing-JS-Functionality/js/header.js
@@ -1,1 +1,23 @@
-// put event handlers for header links here
+$(document).ready(function ready() {
+    var controls = document.querySelector('.controls');
+    var modal = document.getElementById('modal');
+
+    controls.addEventListener('click', function openPopup(event) {
+        if (event.target && event.target.tagName.toLowerCase() == 'a') {
+            event.preventDefault();
+            // event.stopPropagation();
+            // event.stopImmediatePropagation();
+
+            var buttonLink = event.target;
+            var url = buttonLink.getAttribute('href');
+
+            console.log(url);
+
+            $.ajax(url, { dataType: 'text' })
+                .then(function response(contents) {
+                    modal.innerHTML = contents;
+                    $(modal).show();
+                });
+        }
+    });
+});
