06e466db2ec0b78f48566cd5f520147ef737726b
Diego Miguel <digznav@gmail.com>
2019-07-31 09:50:23 -0500

    Task 3

--- Organizing-JS-Functionality/js/carousel.js
+++ Organizing-JS-Functionality/js/carousel.js
@@ -1,39 +1,51 @@
-$(document).ready(function ready() {
-
+var Carousel = (function carousel() {
     function scrollLeft(evt) {
         evt.preventDefault();
         evt.stopPropagation();
         evt.stopImmediatePropagation();
-
+        
         if (position > 0) {
             position = Math.max(0, position - 250);
         }
-
+        
         $items.css({ left: `${-position}px` });
     }
-
+    
     function scrollRight(evt){
         evt.preventDefault();
         evt.stopPropagation();
         evt.stopImmediatePropagation();
-
+        
         if (position < maxPosition) {
             position = Math.min(maxPosition,position + 250);
         }
-
+        
         $items.css({ left: `${-position}px` });
     }
-
+    
     var $content = $('[rel=js-carousel] > [rel=js-content]');
     var $items = $content.children('[rel=js-items]');
-    var $left = $('[rel=js-carousel] > [rel=js-controls] > [rel=js-left]');
-    var $right = $('[rel=js-carousel] > [rel=js-controls] > [rel=js-right]');
+    var $left;
+    var $right;
 
     var contentWidth = $content.width();
     var itemsWidth = $items.width();
     var position = 0;
     var maxPosition = (itemsWidth - contentWidth);
+    
+    function init() {
+        $left = $('[rel=js-carousel] > [rel=js-controls] > [rel=js-left]');
+        $right = $('[rel=js-carousel] > [rel=js-controls] > [rel=js-right]');
 
-    $left.on('click', scrollLeft);
-    $right.on('click', scrollRight);
-});
+        $left.on('click', scrollLeft);
+        $right.on('click', scrollRight);
+    }
+
+    var publicAPI = {
+        init
+    };
+
+    return publicAPI;
+})();
+
+$(document).ready(Carousel.init);
--- Organizing-JS-Functionality/js/details.js
+++ Organizing-JS-Functionality/js/details.js
@@ -1,5 +1,5 @@
-$(document).ready(function ready() {
-    var $items = $('[rel=js-carousel] > [rel=js-content] > [rel=js-items]');
+var Details = (function details() {
+    var $items;
     var $content = $('[rel=js-details]');
     
     function loadPerson(event) {
@@ -12,15 +12,16 @@ $(document).ready(function ready() {
             });
     }
 
-    $items.on('click', '.item', loadPerson);
+    function init() {
+        $items = $('[rel=js-carousel] > [rel=js-content] > [rel=js-items]');
+        $items.on('click', '.item', loadPerson);
+    }
 
-    // on click of a carousel item, do an Ajax request for
-    // the "details/2.html" (or whatever) file for the person
-    // clicked, and load those contents into the `$content` div.
+    var publicAPI = {
+        init
+    };
 
-    // hint: you will probably want to use "event propagation"
-    // (aka "event delegation"), by attaching a single event
-    // handler the `$content` element rather than individual
-    // event handlers to each item in the carousel.
+    return publicAPI;
+})();
 
-});
+$(document).ready(Details.init);
