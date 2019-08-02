71e2d1283087419f104465713a503a8f9a56ae0f
Angel Ponce <angelux.07@gmail.com>
2019-08-02 09:09:56 -0500

    Priscila Changes

--- Organizing-JS-Functionality/js/details.js
+++ Organizing-JS-Functionality/js/details.js
@@ -1,15 +1,28 @@
-$(document).ready(function(){
+var Details = (function() {
+	var $content;
 
-	var $items = $("[rel=js-carousel] > [rel=js-content] > [rel=js-items]");
-	var $content = $("[rel=js-details]");
+	function loadPerson(ID) {
+		$.ajax('details/' + ID + ".html", {
+			dataType: 'text'
+		})
+		.then(function (contents) {
+			$content.html(contents);
+		});
+	}
 
-	// on click of a carousel item, do an Ajax request for
-	// the "details/2.html" (or whatever) file for the person
-	// clicked, and load those contents into the `$content` div.
+	function selectPerson(e) {
+		e.preventDefault();
 
-	// hint: you will probably want to use "event propagation"
-	// (aka "event delegation"), by attaching a single event
-	// handler the `$content` element rather than individual
-	// event handlers to each item in the carousel.
+		var ID = $(e.target).attr('data-person');
 
-});
+		EVT.emit('person-selected', ID);
+	}
+
+	function init() {
+		$content = $("[rel=js-details]");
+		$content.on('click', '[rel=js-select-person]', selectPerson);
+		EVT.on('person-selected', loadPerson);
+	}
+
+	EVT.on('init', init);
+})();
--- Organizing-JS-Functionality/js/header.js
+++ Organizing-JS-Functionality/js/header.js
@@ -1,1 +1,28 @@
-// put event handlers for header links here
+var Header = (function() {
+    var $modal;
+
+    function headerLinkClicks (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        e.stopImmediatePropagation();
+
+        var $target = $(e.target);
+        var url = $target.attr('href');
+
+        $.ajax(url, { dataType: 'text' })
+        .then(function(contents) {
+            $modal.html(contents).show();
+        });
+    }
+
+    function init() {
+        var $controls = $('[rel="js-controls"]');
+        $modal = $('#modal');
+
+        $controls.on('click', 'a', headerLinkClicks);
+    }
+
+    return {
+        init
+    };
+})();
