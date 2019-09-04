57061d14cacd017bad47c1ebc79e742e66bf99ae
Diego Miguel <digznav@gmail.com>
2019-09-04 09:59:53 -0500

    Ex lists reduce

--- functional-light-v3/lists/ex.js
+++ functional-light-v3/lists/ex.js
@@ -1,15 +1,5 @@
 "use strict";
 
-// Put your code here! :)
-
-// function no1() {
-//     return 2;
-// }
-
-// function no2() {
-//     return 9;
-// }
-
 function num(value) {
     return function fn() {
         return value;
@@ -25,12 +15,9 @@ function add2(fn1, fn2) {
 }
 
 function addn(fns) {
-
-    if (fns.length <= 1) return add2(num(0), fns[0]);
-
-    var result = add2(fns[0], fns[1]);
-
-    return result + addn(fns.slice(2));
+    return fns.reduce(function reducer(result, fn) {
+        return add2(num(result), fn);
+    }, 0);
 }
 
 addn([num(1), num(2), num(3), num(4), num(5)]);
